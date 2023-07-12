21dc973f384b7d758a7357e8292d76ff5ea61405
TatsuKenshi <rockinbg@gmail.com>
2023-07-12 16:18:02 +0200

    put and delete requests

--- working-lessons/14-express-put.js
+++ working-lessons/14-express-put.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-let { people } = require("./data");
+let { people } = require("../data");
 
 // static assets
-app.use(express.static("./methods-public"));
+app.use(express.static("../methods-public"));
 
 // parse the html form data
 app.use(express.urlencoded({ extended: false }));
@@ -56,7 +56,7 @@ app.put("/api/people/:id", (req, res) => {
 
   if (!person) {
     return res
-      .status(404)
+      .status(400)
       .json({ success: false, msg: `No person with id ${id}` });
   }
   const newPeople = people.map((person) => {
@@ -66,21 +66,6 @@ app.put("/api/people/:id", (req, res) => {
     return person;
   });
   res.status(200).json({ success: true, data: newPeople });
-});
-
-// delete
-app.delete("/api/people/:id", (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id));
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `No person with id ${req.params.id}` });
-  }
-  const newPeople = people.filter(
-    (person) => person.id !== Number(req.params.id)
-  );
-  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
